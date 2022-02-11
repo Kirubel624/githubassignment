5af699ff3f5a80785502834d573ffd86dc533b17
Kirubel berhanu <kirubelberhanu@gmail.com>
2022-02-12 00:29:27 +0300

    difference function added

--- calculator.cpp
+++ calculator.cpp
@@ -5,3 +5,15 @@
 #include<iomanip>
   char op;
 using namespace std;
+void diff()
+    {
+     int diff;
+     int n1,n2;
+     cout<<"enter two numbers to find their difference:\n\n";
+     cout<<"enter first number:";
+     cin>>n1;
+     cout<<"\nenter second number:";
+     cin>>n2;
+     diff=n1-n2;
+     cout<<"\ndifference is:"<<diff<<endl<<endl;
+     }
