347039c36b8e942451281257eedef5c29729a1e5
MariamawitF <98666963+MariamawitF@users.noreply.github.com>
2022-02-11 15:38:07 -0800

    Update calculator.cpp

--- calculator.cpp
+++ calculator.cpp
@@ -98,18 +98,6 @@ void fact()
 
            }
            
-           void expo()
-     {
-          long double res=1,p;
-     double e=2.718281828;
-     cout<<"enter power of exponential function:";
-     cin>>p;
-     for(int i=1;i<=p;i++)
-     {
-      res=e*res;
-     }
-      cout<<" e^ "<<p<<" is :"<<res<<endl;
-
-           }
+      
 
 
