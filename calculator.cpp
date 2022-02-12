21d714dd81b89c721cf08502b462bbf08a61f709
MariamawitF <harni15fekade@gmail.com>
2022-02-11 19:41:50 -0800

    factorial method added

--- calculator.cpp
+++ calculator.cpp
@@ -54,3 +54,17 @@ void diff()
      cout<<"product is:"<<pro<<endl<<endl;
 
      }
+     
+     void fact()
+     {
+      long int f=1;
+      int c=1,n;
+      cout<<"enter number to find its factorial:";
+      cin>>n;
+      while(c<=n)
+      {
+                 f=f*c;
+                 c+=1;
+      }
+                 cout<<"\nfactorial of "<<n<<" is :"<<f<<endl;
+      }
